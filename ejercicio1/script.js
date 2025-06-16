8566da137f8f1515145e7c14a6b2e35ccfb9e0e8
Franklin <adiel411@gmail.com>
2025-06-15 19:45:14 -0600

    :tada::sparkles: Agregando ejercicio 1

---
+++ ejercicio1/script.js
@@ -0,0 +1,6 @@
+function verificarMayorEdad(edad) {
+  const mensaje = edad >= 18 ? "La persona es mayor de edad." : "La persona NO es mayor de edad.";
+  console.log(mensaje);
+}
+
+verificarMayorEdad(26)
